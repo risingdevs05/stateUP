2f72281dcb6c82ba9c00946ae4a09e205e76b92b
risingdevs <risingdevs01@gmail.com>
2026-03-22 10:50:25 +0530

    feat: initial commit

---
+++ src/features/home/ExploreCards.jsx
@@ -0,0 +1,12 @@
+// Choose Your Experience (Interactive Feel)
+
+// Instead of dumping info, guide the user.
+
+// 4 clickable cards:
+
+// 🛕 Spiritual
+// 🍛 Food
+// 🏛️ History
+// 🌿 Nature
+// something like this we will do here!
+//2nd section
